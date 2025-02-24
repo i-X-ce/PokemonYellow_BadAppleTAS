3307c38cd5ae4a0e7b9f5aee2a7b6a3c96059200
i-X-ce <iceci2804@gmail.com>
2025-02-25 04:26:10 +0900

    ⚗️ ボリュームも弄ってみる実験

--- src/asm/soundtest.asm
+++ src/asm/soundtest.asm
@@ -4,7 +4,13 @@ SECTION "Test",ROM0
 load "ACE", wramx[$da00]
 
 main:
-    ; Hblankに合わせてFF30を書き換えてみる
+    ; ボリュームは2サンプルごとに変更する
+    ld a, $0a 
+    ld [$0000], a 
+    ld a, $01 
+    ld [$6000], a 
+    ld a, $03 
+    ld [$4000], a 
     di 
     ld a, $80  
     ldh [$ff00+$1a], a
@@ -12,41 +18,319 @@ main:
     ldh [$ff00+$1c], a
     xor a  
     ldh [$ff00+$1b], a
+    ldh [$ff00+$05], a
+    ldh [$ff00+$0f], a
+    ld a, $c7 ; 262144 / 57 Hz (2lineに一回)
+    ldh [$ff00+$06], a
+    ld a, $05 
+    ldh [$ff00+$07], a
     ld a, $1c ;周波数
     ldh [$ff00+$1d], a
     ld a, $87 ;周波数
     ldh [$ff00+$1e], a
 
-.input_loop2
+    ld hl, $a000 - 16
+.wait_bloop
+    ld b, $10
+.wait_loop 
+    ldh a, [$ff00+$0f]
+    bit 2, a
+    jr z, .wait_loop
     xor a  
-    ld bc, $20ff
-.input_loop3
-    push af  
-.input_loop
-    ldh a, [$ff00+$41]
-    and $03
-    jr nz, .input_loop
-    ldh a, [$ff00+$44]
-    cp a, c
-    jr z, .input_loop
-    ld c, a 
-    pop af
-    bit 0, b
-    jr nz, .input_skp 
-    push af 
+    ldh [$ff00+$0f], a
+    ld a, [hli]
+    ldh [$ff00+$24], a
+    dec b 
+    jr nz, .wait_loop
     xor a  
     ldh [$ff00+$1a], a
-    pop af 
-    push af 
-    ldh [$ff00+$30], a
+    ld bc, $1030
+.input_loop
+    ld a, [hli]
+    ld [$ff00+c], a
+    inc c 
+    dec b 
+    jr nz, .input_loop
+    ld a, h
+    cp $c0
+    jr c, .hskp
+    ld hl, $a000 - 16
+.hskp
     ld a, $80 
     ldh [$ff00+$1a], a
-    pop af
-    add $11
-.input_skp
-    dec b
-    jr nz, .input_loop3 
-    jr .input_loop2
+    ldh a, [$ff00+$1e]
+    set 7, a
+    ldh [$ff00+$1e], a
+    jr .wait_bloop
+
+
+;     ; TIMAと同期させて16byteごと一気に書き換えてみる。マスターボリュームの変更も行う
+;     ld a, $0a 
+;     ld [$0000], a 
+;     ld a, $01 
+;     ld [$6000], a 
+;     ld a, $03 
+;     ld [$4000], a 
+;     di 
+;     ld a, $80  
+;     ldh [$ff00+$1a], a
+;     ld a, $20 
+;     ldh [$ff00+$1c], a
+;     xor a  
+;     ldh [$ff00+$1b], a
+;     ldh [$ff00+$0f], a
+;     ld a, $c7 ; 262144 / 57 Hz (2lineに一回)
+;     ldh [$ff00+$05], a
+;     ldh [$ff00+$06], a
+;     ld a, $05 
+;     ldh [$ff00+$07], a
+;     ld a, $1c ;周波数
+;     ldh [$ff00+$1d], a
+;     ld a, $87 ;周波数
+;     ldh [$ff00+$1e], a
+
+;     ld hl, $a000 - 16
+;     .wait_bloop ; bをリセットする
+;     ld b, $10
+
+    
+;     .wait_loop 
+;     ldh a, [$ff00+$05]
+;     cp $e4 ; 半分を超えたらボリュームを変更させる
+;     jr c, .wait_loop
+;     ld a, c 
+;     and a   
+;     call z, half_read
+;     ld c, 1 
+;     ldh a, [$ff00+$0f]
+;     bit 2, a
+;     jr z, .wait_loop
+
+;     xor a  
+;     ldh [$ff00+$0f], a
+;     call half_read
+;     ld c, 0 
+;     dec b 
+;     jr nz, .wait_loop
+;     ldh [$ff00+$1a], a
+;     ld bc, $1030
+; .input_loop
+;     ld a, [hli]
+;     ld [$ff00+c], a
+;     inc c 
+;     dec b 
+;     jr nz, .input_loop
+;     ld a, h
+;     cp $c0
+;     jr c, .hskp
+;     ld hl, $a000
+; .hskp
+    
+;     ld a, $80 
+;     ldh [$ff00+$1a], a
+;     ldh a, [$ff00+$1e]
+;     set 7, a
+;     ldh [$ff00+$1e], a
+;     jr .wait_bloop
+
+; half_read:
+;     ld a, [hli]
+;     bit 0, c
+;     jr nz, .swap_skp
+;     swap a 
+;     dec hl 
+; .swap_skp
+;     and $0f
+;     ld d, a
+;     swap d 
+;     or d  
+;     ldh [$ff00+$24], a  
+;     ret  
+
+
+
+;     ; TIMAと同期させて16byteごと一気に書き換えてみる。うまくいった！ちゃんと鳴った！
+;     ld a, $0a 
+;     ld [$0000], a 
+;     ld a, $01 
+;     ld [$6000], a 
+;     ld a, $03 
+;     ld [$4000], a 
+;     di 
+;     ld a, $80  
+;     ldh [$ff00+$1a], a
+;     ld a, $20 
+;     ldh [$ff00+$1c], a
+;     xor a  
+;     ldh [$ff00+$1b], a
+;     ldh [$ff00+$05], a
+;     ldh [$ff00+$0f], a
+;     ld a, $c7 ; 262144 / 57 Hz (2lineに一回)
+;     ldh [$ff00+$06], a
+;     ld a, $05 
+;     ldh [$ff00+$07], a
+;     ld a, $1c ;周波数
+;     ldh [$ff00+$1d], a
+;     ld a, $87 ;周波数
+;     ldh [$ff00+$1e], a
+
+;     ld hl, $a000
+; .wait_bloop
+;     ld b, $10
+; .wait_loop 
+;     ldh a, [$ff00+$0f]
+;     bit 2, a
+;     jr z, .wait_loop
+;     xor a  
+;     ldh [$ff00+$0f], a
+;     dec b 
+;     jr nz, .wait_loop
+;     ldh [$ff00+$1a], a
+;     ld bc, $1030
+; .input_loop
+;     ld a, [hli]
+;     ld [$ff00+c], a
+;     inc c 
+;     dec b 
+;     jr nz, .input_loop
+;     ld a, h 
+;     and $0f 
+;     or $a0
+;     ld h, a 
+;     ld a, $80 
+;     ldh [$ff00+$1a], a
+;     ldh a, [$ff00+$1e]
+;     set 7, a
+;     ldh [$ff00+$1e], a
+;     jr .wait_bloop
+
+
+
+;     ; TIMAと同期させて書き換えてみる(書き換えはできているけど、音が変わらない。BGBの機能で音を出したり消したりすると音が変わる。なぜ。)
+;     ld a, $0a 
+;     ld [$0000], a 
+;     ld a, $01 
+;     ld [$6000], a 
+;     ld a, $03 
+;     ld [$4000], a 
+;     di 
+;     ld a, $80  
+;     ldh [$ff00+$1a], a
+;     ld a, $20 
+;     ldh [$ff00+$1c], a
+;     xor a  
+;     ldh [$ff00+$1b], a
+;     ldh [$ff00+$05], a
+;     ldh [$ff00+$0f], a
+;     ld a, $c7 ; 262144 / 57 Hz (2lineに一回)
+;     ldh [$ff00+$06], a
+;     ld a, $05 
+;     ldh [$ff00+$07], a
+;     ld a, $1c ;周波数
+;     ldh [$ff00+$1d], a
+;     ld a, $87 ;周波数
+;     ldh [$ff00+$1e], a
+
+;     ld hl, $a000
+; .wait_loop 
+;     ldh a, [$ff00+$0f]
+;     bit 2, a
+;     jr z, .wait_loop
+;     xor a  
+;     ldh [$ff00+$0f], a
+;     ld a, [hli]
+;     ldh [$ff00+$30], a
+;     ld a, h 
+;     and $0f 
+;     or $a0
+;     ld h, a 
+;     jr .wait_loop
+
+    
+
+
+
+
+; 途中で波形を変えて音が変わるのか調べる(変わった)
+;     di 
+;     xor a  
+;     ldh [$ff00+$1a], a
+;     ld a, $20 
+;     ldh [$ff00+$1c], a
+;     xor a  
+;     ldh [$ff00+$1b], a
+;     ld a, $1c ;周波数
+;     ldh [$ff00+$1d], a
+;     ld a, $07 ;周波数
+;     ldh [$ff00+$1e], a
+    
+;     xor a  
+;     ld bc, $1030
+; .input_loop
+;     ld [$ff00+c], a
+;     inc c 
+;     dec b 
+;     jr nz, .input_loop
+
+;     ld a, $87 ;周波数
+;     ldh [$ff00+$1a], a
+;     ldh [$ff00+$1e], a
+;     ld bc, $0000
+; .wait_loop 
+;     dec bc 
+;     ld a, c 
+;     or b  
+;     jr nz, .wait_loop
+;     ld a, $ff 
+;     ldh [$ff00+$30], a
+; .end
+;     jr .end
+
+
+
+;     ; Hblankに合わせてFF30を書き換えてみる
+;     di 
+;     ld a, $80  
+;     ldh [$ff00+$1a], a
+;     ld a, $20 
+;     ldh [$ff00+$1c], a
+;     xor a  
+;     ldh [$ff00+$1b], a
+;     ld a, $1c ;周波数
+;     ldh [$ff00+$1d], a
+;     ld a, $87 ;周波数
+;     ldh [$ff00+$1e], a
+
+; .input_loop2
+;     xor a  
+;     ld bc, $20ff
+; .input_loop3
+;     push af  
+; .input_loop
+;     ldh a, [$ff00+$41]
+;     and $03
+;     jr nz, .input_loop
+;     ldh a, [$ff00+$44]
+;     cp a, c
+;     jr z, .input_loop
+;     ld c, a 
+;     pop af
+;     bit 0, b
+;     jr nz, .input_skp 
+;     push af 
+;     xor a  
+;     ldh [$ff00+$1a], a
+;     pop af 
+;     push af 
+;     ldh [$ff00+$30], a
+;     ld a, $80 
+;     ldh [$ff00+$1a], a
+;     pop af
+;     add $11
+; .input_skp
+;     dec b
+;     jr nz, .input_loop3 
+;     jr .input_loop2
 
 
 
--- src/python/soundtest.py
+++ src/python/soundtest.py
@@ -8,27 +8,111 @@ import scipy.io.wavfile as wav
 script_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_dir)
 
+# 量子化サイズ
+quantizeSize = 120
+# 音声サイズ
+audioSize = 8192
+# 新しいサンプリングレート
+rate = 9198
+
+class Volume: 
+    def __init__(self, volume, mastarVolume):
+        self.volume = volume
+        self.mastarVolume = mastarVolume
+    
+    def getVolume(self): 
+        return self.volume * self.mastarVolume
+    
+    def volumeStr(self):
+        return format(self.volume, 'x')
+    
+    def __str__(self):
+        return f"""Volume: {self.volume}, MastarVolume: {self.mastarVolume}, TotalVolume: {self.getVolume()}"""
+
+
+volumeMap = {}
+for i in range(16):
+    for j in range(1, 9):
+        if volumeMap.__contains__(i * j):
+            continue
+        volumeMap[i * j] = Volume(i, j)
+        
+
+# 最も近いボリュームを取得
+def closestVolume(volume):
+    return min(volumeMap.values(), key=lambda x: abs(x.getVolume() - volume))
+
 audio = AudioSegment.from_file("./music/test.mp3")
 audio = audio.set_channels(1).set_frame_rate(44100)
 
 samples = np.array(audio.get_array_of_samples(), dtype=np.float32)
 
-newRate = 9198
-numSamples = int(len(samples) * newRate / 44100)
+numSamples = int(len(samples) * rate / 44100)
 resampledData = scipy.signal.resample(samples, numSamples)
 
 # resampledData *= 16
 minData = np.min(resampledData)
 maxData = max(abs(minData), abs(np.max(resampledData)))
-quantrizedData = np.floor(((resampledData + abs(minData)) / (maxData + abs(minData))) * 16).astype(np.uint8)
+quantrizedData = np.floor(((resampledData - minData) / (maxData - minData)) * quantizeSize).astype(np.uint8)
 
-wav.write("./music/output.wav", newRate, quantrizedData[:8092])
+# 最も近いボリュームに調整
+for i in range(len(quantrizedData)):
+    quantrizedData[i] = closestVolume(quantrizedData[i]).getVolume()
+
+wav.write("./music/output.wav", rate, quantrizedData[:audioSize])
 print(quantrizedData.shape)
 
 musicFile = open("./music/music.txt", "w")
-start = 0
-for i in range(start, start + 8192):
-    musicFile.write(format(min(quantrizedData[i], 16), 'x'))
+
+# volumeを分けて16進数に変換
+def divisionVolume(volume):
+    V = volumeMap[volume]
+    if V == None:
+        return "0", "0"
+    return format(V.volume, 'x'),  format(V.mastarVolume, 'x')
+
+
+for i in range(audioSize // 32):
+    volumes = ""
+    masterVolumes = ""
+    for j in range(16):
+        V1 = volumeMap[quantrizedData[i * 16 + j * 2]]
+        V2 = volumeMap[quantrizedData[i * 16 + j * 2 + 1]]
+        sum = V1.getVolume() + V2.getVolume()
+        m = round(sum / (V1.volume + V2.volume))
+        volumes += V1.volumeStr() + V2.volumeStr()
+        masterVolumes += format(m - 1, 'x') + format(m - 1, 'x')
+    musicFile.write(volumes + masterVolumes + "\n")
+
+
+
+# for i in range(audioSize // 16):
+#     volumes = ""
+#     masterVolumes = ""
+#     for j in range(1, 16):
+#         v, m = divisionVolume(quantrizedData[i * 16 + j])
+#         volumes += v
+#         masterVolumes += m
+#     v, m = divisionVolume(quantrizedData[i * 16])
+#     volumes += v
+#     masterVolumes += m
+#     musicFile.write(volumes + masterVolumes + "\n")
+
+
+
+# start = 0
+# for i in range(start, start + 8192):
+#     musicFile.write(format(min(quantrizedData[i], 16), 'x'))
+
+# for i in range(0, 512):
+#     s = ""
+#     for j in range(1, 16):
+#         s += format(min(quantrizedData[i * 16 + j], 16), 'x')
+#     s += format(min(quantrizedData[i * 16], 16), 'x')
+#     musicFile.write(s)
+
+
+    
 
 # fps = 60
 # samplesPerFrame = 274
